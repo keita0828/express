8ae04263f528badf908251cb92fd35c808150318
keita0828 <qigongteng29@gmail.com>
2025-02-26 22:19:49 +0900

    add rest api service

--- routes/notes.js
+++ routes/notes.js
@@ -1,32 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-// レスポンスのデータ　（ノート全体）
-const responseObjectDataAll = {
-  textObject1 : {
-    id: 1,
-    title: 'ノート1のサブタイトルです',
-    subTitle: 'ノート1のサブタイトルです',
-    bodyText: 'ノート1の本文です'    
-  },    
-  textObject2 : {
-    id: 2,
-    title: 'ノート2のサブタイトルです',
-    subTitle: 'ノート2のサブタイトルです',
-    bodyText: 'ノート2の本文です'        
-  },
-};
 
-/**
- * メモを全件取得するAPI
- * @returns {Object[]} data
- * @returns {number} data.id - ID
- * @returns {string} data.title - タイトル
- * @returns {string} data.text - 内容
- */
- router.get('/', function (req,res,next) {
-  //　全件取得して返す
-  res.json(responseObjectDataAll);    
+
+// 接続情報を設定
+const { MongoClient } = require("mongodb");
+const uri =  "mongodb+srv://keita:amo0828db@cluster0.wwyxk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const client = new MongoClient(uri);
+
+router.get('/', async (req,res) => {
+// データベース、コレクションを指定
+const database = client.db('notes');
+const client = database.collection('notes');
+
+// idが1のドキュメントを取得
+const query = { id: 2 };
+const note = await notes.findOne(query);
+
+res.json(note.title);
 })
 
 module.exports = router;
